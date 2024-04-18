8174cd42b197921264701ce3d7af454ec84f4201
mbruce10 <megleigh2462@gmail.com>
2024-04-18 07:07:11 -0400

    Added random image function

--- js/script.js
+++ js/script.js
@@ -9,7 +9,27 @@ const getImage = async function () {
         "https://picsum.photos/v2/list?limit=100"
     );
     const images = await res.json();
-    console.log(images);
+    // console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    // console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    // console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+ 
+button.addEventListener("click", function () {
+    getImage();
+})
+
